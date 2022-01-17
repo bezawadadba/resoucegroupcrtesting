6109414fce8f970564dfd645c270892a067cc496
bezawadadba <bezawadadba@gmail.com>
2022-01-17 05:59:40 -0800

    Added TAG2

--- testingwithoutterraformcloud/local.tf
+++ testingwithoutterraformcloud/local.tf
@@ -10,7 +10,7 @@ locals {
     Service = local.service_name
     Owner   = local.owner
     Tag1 = "Terraform-Cloud-Demo1"
-   # Tag2 = "Terraform-Cloud-Demo2"
+    Tag2 = "Terraform-Cloud-Demo2"
    # Tag3 = "Terraform-Cloud-Demo3"
   }
 }
